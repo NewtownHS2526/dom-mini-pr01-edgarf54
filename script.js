c068a208628eb0f8d51d39c421e69c692da010d3
Edgar Fontanillas <edgarf54@nycstudents.net>
2026-01-15 14:26:20 -0500

    im done

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ let brown_position = 1;
 // Query Selectors Here
 const blueButton = document.querySelector("#blue-button");
 const blueHorse = document.querySelector("#blue-horse");
-
-
+const pinkButton = document.querySelector("#pink-button");
+const pinkHorse = document.querySelector("#pink-horse");
+const brownButton = document.querySelector("#brown-button");
+const brownHorse = document.querySelector("#brown-horse");
 
 // OnClick Functions Here
 const advanceBlue = (e) => {
@@ -19,17 +21,25 @@ const advanceBlue = (e) => {
   changePosition(blueHorse, blue_position);
 };
 const advancePink = (e) => {
-
+  pink_position += 1;
+  changePosition(pinkHorse, pink_position);
 };
-
+const advanceBrown = (e) => {
+  brown_position += 1;
+  changePosition(brownHorse, brown_position);
+};
 
 
 // Check for a winner
 const checkWinner = (position, color) => {
-
+  if (position === 5) {
+    winnerText.innerHTML = color + " is the Winner!";
+  }
 };
 
 
 
 // Event Listeners Here
 blueButton.addEventListener("click", advanceBlue);
+pinkButton.addEventListener("click", advancePink);
+brownButton.addEventListener("click", advanceBrown);
